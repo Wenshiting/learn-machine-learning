90b4720966e41e29a2422dbc0cc49dff83deb774
Wenshiting <33195738+Wenshiting@users.noreply.github.com>
2019-05-02 10:15:01 +0800

    Update decisitiontreetest.py

--- decisitiontreetest.py
+++ decisitiontreetest.py
@@ -5,6 +5,7 @@ from sklearn.metrics import (brier_score_loss, precision_score, recall_score,f1_
 from sklearn.model_selection import learning_curve
 import numpy as np
 import matplotlib.pyplot as plt
+
 iris = load_iris()
 iris_X = iris.data   #x有4个属性，共有600个样本点
 iris_y = iris.target #y的取值有3个，分别是0,1,2
@@ -12,11 +13,13 @@ X_train, X_test, y_train, y_test = train_test_split(iris_X, iris_y, test_size=0.
 clf = tree.DecisionTreeClassifier()
 clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
+
 print(clf.predict(X_test))
 print(y_test)
 print("\tPrecision: %1.3f" % precision_score(y_test, y_pred, average="micro"))
 print("\tRecall: %1.3f" % recall_score(y_test, y_pred, average="micro"))
 print("\tF1: %1.3f\n" % f1_score(y_test, y_pred, average="micro"))
+
 train_sizes, train_loss, test_loss = learning_curve( clf, X_train, y_train, cv=10,
     train_sizes=[0.1, 0.25, 0.5, 0.75, 1])
 train_loss_mean = -np.mean(train_loss, axis=1)
